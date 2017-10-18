56faa8c265bf85b4c1493b7eeb884a82b00f5e81
Jasper <huangqisheng1@xiaoniu66.com>
2017-10-18 15:52:19 +0800

    更新目录

---
+++ CYUtilProject/CYShareSDK/CustomShare/CYBaseShare.h
@@ -0,0 +1,46 @@
+//
+//  CYShareBaseUtil.h
+//  CYUtilProject
+//
+//  Created by HuangQiSheng on 3/22/16.
+//  Copyright © 2016 Charry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class CYShareModel;
+
+typedef void (^CYThirdPartyLoginCallback)(NSInteger code, NSString *msg, NSDictionary *resultDic);
+typedef void (^CYShareCallback)(NSInteger code, NSString *msg);
+
+@interface CYBaseShare : NSObject
+
+@property (nonatomic, strong, readonly) NSString *appId;
+//@property (nonatomic, strong, readonly) NSString *appKey;
+
+@property (nonatomic, copy) CYShareCallback shareCallback;
+
+#pragma mark - share
+// Subclass should implements this method to implement the share action
+- (void)share:(CYShareModel *)model
+     callback:(CYShareCallback)callback;
+
+#pragma mark - app info
+// 注册appid和appKey
+- (void)registerWithAppId:(NSString *)appId;
+
+#pragma mark - api
+/**
+ *  打开App
+ *
+ *  @return YES打开成功，NO打开失败
+ */
++ (BOOL)openApp;
+
+/*! @brief 检查App是否已被用户安装
+ *
+ * @return 已安装返回YES，未安装返回NO。
+ */
++ (BOOL)appInstalled;
+
+@end
